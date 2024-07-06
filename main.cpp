6d7a863b982643b8188d7e38c1dc974b58dba40a
MadhanM123 <madhangopalvel@gmail.com>
2024-07-06 16:47:59 -0700

    main test code and makefile

--- main.cpp
+++ main.cpp
@@ -1,3 +1,13 @@
 #include "tgaimg.h"
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor red = TGAColor(255, 0, 0, 255);
+
+int main(int argc, char** argv){
+    TGAImg img(100, 100, TGAImg::RGB);
+    img.set(52, 41, red);
+
+    img.flip_vert();
+    img.write_tga_file("out.tga");
+    return 0;
+}
